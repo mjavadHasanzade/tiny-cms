a3f764da0edd746f6c9902511dedb59b6543514a
Mjavad Hasanzade <hjavad522@gmail.com>
2022-06-26 22:44:35 +0430

    feat: create create setting api

---
+++ pages/api/settings/create.ts
@@ -0,0 +1,20 @@
+import prisma from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../middlewares/auth";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isAuth = auth(req, res);
+  if (!isAuth) return res.status(401).send({ message: "Access Denied !!!" });
+  req.body.userId = req.user.id;
+  console.log(req.body);
+
+  const settings = await prisma.settings.create({ data: req.body });
+
+  res.send({ settings });
+}
