1f2b9a6ce6c10bf41b8368a44c4ea2d529a70d05
Mjavad Hasanzade <hjavad522@gmail.com>
2022-06-26 21:04:19 +0430

    feat: create Auth middleware

---
+++ middlewares/auth.ts
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const auth = (req: NextApiRequest, res: NextApiResponse) => {
+  const token = req.headers["xauth"];
+  if (!token) return null;
+
+  try {
+    const user = jwt.verify(String(token), "tinyCmsJwtKey");
+    req.user = user;
+    return req;
+  } catch (ex) {
+    return null;
+  }
+};
+
+export default auth;
