eb149b570bd8b5be98aa2cc8506cdd5ce787dce8
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-08 14:31:23 +0430

    feat: create slogan APIs

---
+++ pages/api/slogans/[id].ts
@@ -0,0 +1,51 @@
+import prisma from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../middlewares/auth";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isAuth = auth(req, res);
+  if (!isAuth) return res.status(401).send({ message: "Access Denied !!!" });
+
+  if (req.method === "DELETE") {
+    try {
+      await prisma.subContent.deleteMany({
+        where: { sloganId: Number(req.query.id) },
+      });
+      await prisma.slogan.delete({ where: { id: Number(req.query.id) } });
+      res.send({ message: "Slogan Deleted Seccessfully" });
+    } catch (error) {
+      res.status(400).send({ message: "invalid Id", error });
+    }
+  } else {
+    req.body.userId = req.user.id;
+    const { name, title, content, image, userId, subContent } = req.body;
+
+    try {
+      const slogan = await prisma.slogan.update({
+        where: { id: Number(req.query.id) },
+        data: {
+          name,
+          content,
+          image,
+          userId,
+          title,
+          subContent: {
+            connect: subContent,
+          },
+        },
+      });
+      res.send({ message: "Slogan Edited Successfully", slogan });
+    } catch (error) {
+      res.status(400).send({
+        message: "Slogan Did'nt Edit! Something Went Wrong",
+        error,
+      });
+    }
+  }
+}
---
+++ pages/api/slogans/create.ts
@@ -0,0 +1,34 @@
+import prisma from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../middlewares/auth";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isAuth = auth(req, res);
+  if (!isAuth) return res.status(401).send({ message: "Access Denied !!!" });
+  req.body.userId = req.user.id;
+  const { name, title, content, image, userId, subContent } = req.body;
+
+  try {
+    const slogan = await prisma.slogan.create({
+      data: {
+        name,
+        content,
+        image,
+        userId,
+        title,
+        subContent: {
+          create: subContent,
+        },
+      },
+    });
+    res.send({ message: "Slogan Created Successfully", slogan });
+  } catch (error) {
+    res.send({ message: "Slogan Did'nt Create! Something Went Wrong", error });
+  }
+}
