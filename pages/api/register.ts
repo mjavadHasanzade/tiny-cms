3b5241461d7c352086c163353a8673f1c6800e9e
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-08 11:43:06 +0430

    feat: create register api

---
+++ pages/api/register.ts
@@ -0,0 +1,29 @@
+import prisma from "../../lib/prisma";
+import { decode } from "../../utils/bcrypt";
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import { encode } from "../../utils/bcrypt";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { username } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: { username },
+  });
+  if (user) return res.status(400).send({ message: "User Already Exists" });
+
+  req.body.password = await encode(req.body.password);
+
+  const newUser = await prisma.user.create({ data: req.body });
+  console.log(newUser);
+  res.json({
+    token: jwt.sign(newUser, "tinyCmsJwtKey"),
+    message: "Register Successfully",
+  });
+}
