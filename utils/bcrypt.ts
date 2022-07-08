42af2e16862ca5e64ac123e5d0bc918a69c500ae
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-08 11:41:09 +0430

    feat: add encode functionality

--- utils/bcrypt.ts
+++ utils/bcrypt.ts
@@ -4,4 +4,8 @@ function decode(hash: string, password: string) {
   return bcrypt.compareSync(password, hash);
 }
 
-export { decode };
+function encode(password: string) {
+  return bcrypt.hash(password, 10);
+}
+
+export { decode, encode };
