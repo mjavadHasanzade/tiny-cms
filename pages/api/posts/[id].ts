3e505f709cc29d591605c217430e075ddfa67dbf
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-08 18:34:33 +0430

    feat: create posts APIs

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,44 @@
+import prisma from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../middlewares/auth";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isAuth = auth(req, res);
+  if (!isAuth) return res.status(401).send({ message: "Access Denied !!!" });
+
+  if (req.method === "DELETE") {
+    try {
+      await prisma.thread.deleteMany({
+        where: { commentId: Number(req.query.id) },
+      });
+      await prisma.comment.deleteMany({
+        where: { postId: Number(req.query.id) },
+      });
+      await prisma.post.delete({ where: { id: Number(req.query.id) } });
+      res.send({ message: "Slogan Deleted Seccessfully" });
+    } catch (error) {
+      res.status(400).send({ message: "invalid Id", error });
+    }
+  } else {
+    req.body.userId = req.user.id;
+
+    try {
+      const post = await prisma.post.update({
+        where: { id: Number(req.query.id) },
+        data: req.body,
+      });
+      res.send({ message: "Post Edited Successfully", post });
+    } catch (error) {
+      res.status(400).send({
+        message: "Post Did'nt Edit! Something Went Wrong",
+        error,
+      });
+    }
+  }
+}
---
+++ pages/api/posts/create.ts
@@ -0,0 +1,24 @@
+import prisma from "../../../lib/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
+import auth from "../../../middlewares/auth";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const isAuth = auth(req, res);
+  if (!isAuth) return res.status(401).send({ message: "Access Denied !!!" });
+  req.body.userId = req.user.id;
+
+  try {
+    const post = await prisma.post.create({
+      data: req.body,
+    });
+    res.send({ message: "Post Created Successfully", post });
+  } catch (error) {
+    res.send({ message: "Post Did'nt Create! Something Went Wrong", error });
+  }
+}
