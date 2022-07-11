52ed708033043f4acebff1d384a946fb7d6007e7
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-12 00:49:52 +0430

    feat: add new fonts to project

--- components/atoms/seo.tsx
+++ components/atoms/seo.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import React, { FC } from "react";
 import Head from "next/head";
 
@@ -13,8 +14,18 @@ const Seo: FC<ISeo> = ({ title, children, favicon = "/imgs/NSA.png" }) => {
       <title>{title}</title>
       {children}
       <link rel="icon" href={favicon} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin={"crossOrigin"}
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Edu+SA+Beginner:wght@500;700&family=Poppins:wght@300&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 };
 
-export default Seo
+export default Seo;
