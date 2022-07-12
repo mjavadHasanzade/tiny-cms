6f450af0a9588fa76eafd0832f746e44e6ca2b36
Mjavad Hasanzade <hjavad522@gmail.com>
2022-07-12 23:31:31 +0430

    feat: change admin page route

---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@organisms/layout";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Layout translations={""} isLogin={true}>
+      Home Page
+    </Layout>
+  );
+};
+
+export default Home;
