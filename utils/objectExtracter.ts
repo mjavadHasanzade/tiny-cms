549dcf3abd373b874f14d24f6feb83468ea947d3
Mjavad Hasanzade <hjavad522@gmail.com>
2022-08-11 22:09:09 +0430

    feat: create object extracter function

---
+++ utils/objectExtracter.ts
@@ -0,0 +1,7 @@
+function objectExtracter(obj: {}, minus: Array<string>): Array<string> {
+    let objects = Object.keys(obj);
+    objects = objects.filter((el) => !minus.includes(el));
+    return objects;
+  }
+  
+  export default objectExtracter;
