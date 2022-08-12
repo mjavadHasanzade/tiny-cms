ac566534f969ff934080dc4d5966ad2c65b90f22
Mjavad Hasanzade <hjavad522@gmail.com>
2022-08-12 20:44:20 +0430

    feat: create camel case utility

---
+++ utils/camel-case.ts
@@ -0,0 +1,12 @@
+const camelCase = (str: string) => {
+  return str
+    .replace(/\s(.)/g, function ($1) {
+      return $1.toUpperCase();
+    })
+    .replace(/\s/g, "")
+    .replace(/^(.)/, function ($1) {
+      return $1.toLowerCase();
+    });
+};
+
+export default camelCase;
