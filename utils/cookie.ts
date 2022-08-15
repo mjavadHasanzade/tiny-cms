8243db3247db7837d4b31147fc9cf5554d1121bc
Mjavad Hasanzade <hjavad522@gmail.com>
2022-08-15 09:06:44 +0430

    feat: add delete cookie functionallity

--- utils/cookie.ts
+++ utils/cookie.ts
@@ -24,3 +24,7 @@ export const getCookie = (cookieName: string, cookieStr: string) => {
   }
   return "";
 };
+
+export const deleteCookie = (cookieName: string) => {
+  document.cookie = `${cookieName}= ; expires = Thu, 01 Jan 1970 00:00:00 GMT`;
+};
