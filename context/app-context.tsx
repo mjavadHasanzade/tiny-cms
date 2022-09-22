f2b476ed8396e9fc439bb516c42d32a34e3456f9
Mjavad Hasanzade <hjavad522@gmail.com>
2022-09-22 21:24:25 +0330

    feat: adding loader between page

--- context/app-context.tsx
+++ context/app-context.tsx
@@ -1,5 +1,11 @@
-import React, { createContext, FC, useContext, useState } from "react";
-
+import React, {
+  createContext,
+  FC,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import Router from "next/router";
 interface IAppProvider {
   children: any;
 }
@@ -12,7 +18,27 @@ interface ICtx {
 const AppContext = createContext<ICtx | any>({});
 
 const AppProvider: FC<IAppProvider> = ({ children }) => {
-  const [loaderActive, setLoaderActive] = useState(false);
+  const [loaderActive, setLoaderActive] = useState(true);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => setLoaderActive(true));
+    Router.events.on("routeChangeComplete", () => setLoaderActive(false));
+    Router.events.on("routeChangeError", () => setLoaderActive(false));
+    return () => {
+      Router.events.off("routeChangeStart", () => setLoaderActive(true));
+      Router.events.off("routeChangeComplete", () => setLoaderActive(false));
+      Router.events.off("routeChangeError", () => setLoaderActive(false));
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [Router.events]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setTimeout(() => {
+        setLoaderActive(false);
+      }, 2000);
+    }
+  }, []);
 
   //? functions
 
