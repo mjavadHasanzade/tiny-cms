fddf0d9de39ef02f01644a953385da2b2ccab43d
Mjavad Hasanzade <hjavad522@gmail.com>
2022-11-25 11:42:09 +0330

    feat: add image selector fullwidth

--- admin-components/atoms/image-selector.tsx
+++ admin-components/atoms/image-selector.tsx
@@ -1,42 +1,75 @@
 /* eslint-disable @next/next/no-img-element */
 import theme from "@utils/admin/theme";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { GoFile } from "react-icons/go";
-import { BiFullscreen, BiPencil } from "react-icons/bi";
-import { FiTrash2 } from "react-icons/fi";
+import { BiFullscreen } from "react-icons/bi";
+import { FiPlus, FiTrash2 } from "react-icons/fi";
 
 type Props = {
   img?: string;
+  name?: string;
   children?: React.ReactNode;
   onClick?: (event: React.MouseEvent<HTMLElement>) => void;
   onClickDelete?: (event: React.MouseEvent<HTMLElement>) => void;
 };
 
 const ImageSelector = (props: Props) => {
+  const [fullWidth, setFullWidth] = useState(false);
+  const handleFullWidth = () => {
+    setFullWidth(true);
+  };
+
   return (
-    <ImageSelectorST className="image-selector">
-      <>
-        {!props.img ? (
-          <div className="uploadIcon" onClick={props.onClick}>
-            <GoFile className="icon" />
-          </div>
-        ) : (
-          <ImageContainer>
-            <img src={props.img} alt="image" />
-            <div className="actions">
-              <i>
-                <BiFullscreen className="icon edit" />
-              </i>
-              <i onClick={props.onClickDelete}>
-                <FiTrash2 className="icon delete" />
-              </i>
+    <>
+      {props.img && fullWidth && (
+        <>
+          <FullWidthImage img={props.img} />
+          <BackgroundCloserST
+            onClick={() => setFullWidth(false)}
+          ></BackgroundCloserST>
+          <FullWidthMenuBar className="menuBarImageSelector">
+            <div className="text">{props.name ? props.name : props.img.split('/').pop()}</div>
+            <span className="icon" onClick={() => setFullWidth(false)}>
+              <FiPlus />
+            </span>
+          </FullWidthMenuBar>
+        </>
+      )}
+      <ImageSelectorST className="image-selector">
+        <>
+          {!props.img ? (
+            <div className="uploadIcon" onClick={props.onClick}>
+              <GoFile className="icon" />
             </div>
-          </ImageContainer>
-        )}
-        {props.children}
-      </>
-    </ImageSelectorST>
+          ) : (
+            <ImageContainer>
+              <img src={props.img} alt="image" />
+              <div className="actions">
+                <i>
+                  <BiFullscreen
+                    className="icon edit"
+                    onClick={() => handleFullWidth()}
+                  />
+                </i>
+                <i onClick={props.onClickDelete}>
+                  <FiTrash2 className="icon delete" />
+                </i>
+              </div>
+            </ImageContainer>
+          )}
+          {props.children}
+        </>
+      </ImageSelectorST>
+    </>
+  );
+};
+
+const FullWidthImage = ({ img }: { img: string }) => {
+  return (
+    <FullWidthST className="fullWidthImage">
+      <img src={img} alt="image" />
+    </FullWidthST>
   );
 };
 
@@ -95,5 +128,61 @@ const ImageContainer = styled.div`
       cursor: pointer;
       margin: 0 0.5rem;
     }
+  }
+`;
+
+const FullWidthST = styled.div`
+  position: fixed;
+  left: 50%;
+  top: calc(50% + 30px);
+  transform: translate(-50%, -50%);
+  object-fit: cover;
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(80% - 60px);
+
+  img {
+    border-radius: 20px;
+    max-width: 100%;
+    height: 100%;
+  }
+`;
+
+const BackgroundCloserST = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  background-color: #0007;
+  z-index: 1;
+`;
+
+const FullWidthMenuBar = styled.div`
+  height: 60px;
+  width: 100%;
+  background-color: #fff;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  padding: 1.5rem;
+  position: fixed;
+  top: 0;
+  width: 100vw;
+  left: 0;
+  z-index: 2;
+
+  .icon {
+    transform: rotate(45deg);
+    display: inline-block;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+  .text {
+    font-size: 1.1rem;
+    font-weight: 400;
+    margin: 0 auto;
   }
 `;
